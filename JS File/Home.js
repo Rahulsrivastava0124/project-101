9f67229f8e77184afd9b053235b2cdb2e49c13e7
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2023-05-18 12:52:23 +0530

    Navbar is created but not responsive

--- JS File/Home.js
+++ JS File/Home.js
@@ -1,8 +1,13 @@
 console.log("all ok");
 function Change_Nav_Color(Clickid) {
-    console.log(Clickid);
    let clicked_data= document.getElementById(Clickid);
    let color=clicked_data.attributes.name.value;
    let change_color=document.getElementById("Default_color")
    change_color.style.backgroundColor=color;
+   if (color=='Black') {
+    let length=document.getElementsByClassName("menu_link").length;
+    for (let index = 0; index < length; index++) {
+        document.getElementsByClassName("menu_link")[index].style.color="white";
+    }
+   }
 }
