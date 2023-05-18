7c7367c1c79ad9d7cb1d65620aff9e5f3feeadb2
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2023-05-18 06:48:39 +0530

    navbar color change effect us created but menu text color is not match in navbar color

--- JS File/Home.js
+++ JS File/Home.js
@@ -1,1 +1,8 @@
 console.log("all ok");
+function Change_Nav_Color(Clickid) {
+    console.log(Clickid);
+   let clicked_data= document.getElementById(Clickid);
+   let color=clicked_data.attributes.name.value;
+   let change_color=document.getElementById("Default_color")
+   change_color.style.backgroundColor=color;
+}
